4dbbc9321264d8352f19a731b927e23f6f48385e
Shan G <shanmugavel.g@gmail.com>
2022-03-06 10:18:03 -0500

    Updstes

---
+++ hello.js
@@ -0,0 +1,47 @@
+// console.log('Hello world')
+
+// var name="Sri"
+
+// var age = 10
+// console.log(age)
+
+// var num1= 10
+// var num2 = 30
+// console.log(num1+num2)
+
+// console.log(100*43)
+
+// var num=100
+// console.log(num++)
+// console.log(num)
+// num++;
+
+// console.log(num)
+// num++
+// console.log(num)
+// num--
+// console.log(num)
+
+// console.log(++num)
+
+// console.log(13*6)
+// console.log(10 < 100)
+
+// var num = 45
+// if(num < 100) {
+//     console.log('Number is lesser than 100')
+// }
+
+// for(var i=0; i<10; i++) {
+//     console.log('The number is '+i+' now')
+// }
+
+
+for(var i =9; i<5; i++) {
+    console.log(' hello...')
+}
+
+
+// if() {
+
+//}
